7188e78d0a1c38704050ec52fc4fd2514d3096d7
Maxi Cejas <maxicejas12354@gmail.com>
2024-08-02 15:50:54 -0300

    Combination of array with timeline

--- src/App.jsx
+++ src/App.jsx
@@ -7,42 +7,28 @@ gsap.registerPlugin(ScrollTrigger)
 function App() {
   
   useEffect(()=>{
-    let tl = gsap.timeline({
-      scrollTrigger :{
-        trigger: ".container",
-        start: "top top",
-        end: "+=2500",
-        pin: true,
-        pinSpacer: false,
-        markers: true,
-        scrub: 3,
-      }
+    
+    const boxes = gsap.utils.toArray('.box')
+    boxes.forEach((box)=>{
+      let tl = gsap.timeline()
+      tl.to(box, {y: 100, duration: 2})
+      .to(box,{y:0, duration: 2})
     })
-    tl.to(".box-1",{
-      top: "100vh",
-      duration: 2
-    })
-    tl.to(".box-2",{
-      left: "100vw",
-      duration: 2
-    })
-
   },[])
+ 
 
+  return (  
+    <div className="container">
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+    </div>
 
-
-  return (
-  <div className="container">
-    <div className="box-1">
-      <img src="/1.jpg" alt="" />
-    </div>
-    <div className="box-2">
-      <img src="/2.jpg" alt="" />
-    </div>
-    <div className="box-3">
-      <img src="/3.jpg" alt="" />
-    </div>
-  </div>
   );
 }
 
